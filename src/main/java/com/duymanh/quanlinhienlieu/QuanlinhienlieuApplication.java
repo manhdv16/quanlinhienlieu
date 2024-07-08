3debcef2dfa06a98afaeacc206fa8fa6d033b9bd
manhdv16 <manhvp0412@gmail.com>
2024-07-08 17:21:50 +0700

    first commit

---
+++ src/main/java/com/duymanh/quanlinhienlieu/QuanlinhienlieuApplication.java
@@ -0,0 +1,13 @@
+package com.duymanh.quanlinhienlieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlinhienlieuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanlinhienlieuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duymanh/quanlinhienlieu/QuanlinhienlieuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duymanh.quanlinhienlieu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlinhienlieuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
